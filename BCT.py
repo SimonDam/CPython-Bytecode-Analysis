08376e8460109c3d127ede0900f5bb05b18204be
Unknown <simon9400@live.dk>
2020-10-17 19:26:09 +0200

    temp refactor commit

--- BCT.py
+++ BCT.py
@@ -13,7 +13,7 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 import jsonpickle
-
+#from utils.setup import setup
 class Bytecode_stat:
     __valid_bytecodes = {1,2,3,4,5,6,9,10,11,12,15,16,17,19,20,22,23,24,25,26,27,28,29,50,51,52,53,54,55,56,57,59,60,61,62,63,64,
                          65,66,67,68,69,70,71,72,73,75,76,77,78,79,81,82,83,84,85,86,87,88,89,90,90,91,92,93,94,95,96,97,98,100,101,
---
+++ utils/setup.py
@@ -0,0 +1,68 @@
+import pyRAPL
+import os
+import argparse
+from warnings import warn
+import platform
+
+def ensure_BCT_dir(BCT_path):
+    while True:
+        if not os.path.isdir(BCT_path):
+            answer = input("The directory specified in bcc.txt does not exist. Do you want to create it? (Y/N)").upper()
+            if answer in ("Y", "YES"):
+                try:
+                    os.makedirs(BCT_path)
+                except OSError as error:
+                    print(f"Unable to create directory at {BCT_path}. {error}")
+                else:
+                    return BCT_path
+        else:
+            return BCT_path
+        BCT_path = input("Please specify a folder to write bytecodes to: ")
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("source_dir",
+                        help="path to the directory of the .py-files to be run.")
+    parser.add_argument("-v", "--verbose", action="store_true",
+                        help="print running statistics to the console.")
+    parser.add_argument("-f", "--force", action="store_true", 
+                        help="measure all programs regardless if they have already been measured.")
+    return parser.parse_args()
+
+def ensure_compilied():
+    # TODO implement this
+    warn("ensure_compiled is not implemented.")
+
+def get_BCT_path():
+    os.chdir(os.path.abspath("./Python-BCT"))
+    BCT_path = ""
+    if os.path.isfile("bcc.txt"):
+        with open("bcc.txt", 'r') as bct_file:
+            BCT_path =  bct_file.readline()
+            BCT_path = ensure_BCT_dir(BCT_path)
+    else:
+        with open("bcc.txt", 'w') as bct_file:
+            BCT_path = ensure_BCT_dir("")
+            bct_file.write(BCT_path)
+    os.chdir(os.path.abspath(".."))
+    return BCT_path
+
+def getPython_Paths():
+    if(platform.platform().startswith("Windows")):
+        return os.path.abspath("./Python/python.bat"), os.path.abspath("./Python-BCT/Python.bat")
+    else:
+        return os.path.abspath("./Python/python"), os.path.abspath("./Python-BCT/python")
+
+
+def setup():
+    pyRAPL.setup()
+    ensure_compilied()
+    vanilla_path, bc_path = getPython_Paths()
+    BCT_path = get_BCT_path()
+    
+    # We handle the case there the user did or did not add the path seperator to their input.
+    args = get_args()
+    if not args.source_dir.endswith(os.sep):
+        args.source_dir += os.sep
+    return vanilla_path, bc_path, args, BCT_path
